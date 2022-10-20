1425a7bf923724539d6caabb3f928521a07b9f96
Adrian Chan <adrianchanuk@gmail.com>
2022-10-20 20:26:45 +0800

    .

--- src/App.js
+++ src/App.js
@@ -264,11 +264,6 @@ function App() {
 
     };
 
-
-    $(document).ready(function(){
-      initGSAPVideo(gsap, {vidSelector: '#promo-vid-mobile', trigger: '.promo-vid-mobile-section', pin: true, end: 'bottom+=50% bottom', scrub: 2})
-    })
-
     window.addEventListener('scroll', handleScroll);
     
     initGSAPVideo(gsap, {vidSelector: '#promo-vid-mobile', trigger: '.promo-vid-mobile-section', pin: true, end: 'bottom+=50% bottom', scrub: 2})
