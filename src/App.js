e6e49fd2ea9d624961670f22b59e399db22e33c3
Adrian Chan <adrianchanuk@gmail.com>
2022-10-20 12:32:05 +0800

    Updates

--- src/App.js
+++ src/App.js
@@ -254,7 +254,7 @@ function App() {
 
   
     const handleScroll = event => {
-      if (isColliding('nav', '.hero-section') || isColliding('nav', '.video-nft-section') || isColliding('nav', '.video-sun-section') || isColliding('nav', '.video-2-section') || isColliding('nav', '.video-4-section') || isColliding('nav', '.rounded-section') || isColliding('nav', '.image-slider') ) {
+      if (isColliding('nav', '.hero-section') || isColliding('nav', '.video-nft-section') || isColliding('nav', '.video-sun-section') || isColliding('nav', '.video-2-section') || isColliding('nav', '.video-4-section') || isColliding('nav', '.rounded-section') || isColliding('nav', '.image-slider') || isColliding('nav', '.motoGp-promo-section')  || isColliding('nav', '.icons-section') ) {
       $('.main-nav').css('background-color', 'black')
   } else {
     $('.main-nav').css('background-color', 'transparent')
@@ -629,7 +629,7 @@ function App() {
         <div className='rounded-full bg-[#E3DDD4] w-[184vw] h-[184vw] relative left-[-42vw]'></div>
       </div>
       <section className='flex flex-col items-center h-screen py-16 text-center text-[#E3DDD4] gallery-section'>
-        <div className='flex flex-col items-center'>
+        <div className='flex flex-col items-center gallery-wrapper'>
         <h2 className='text-4xl max-w-[30rem] gallery-header'>Gryfyn is everything you need to unlock <span className='text-[#FFCC31]'>endless possibilities</span>.</h2>
         <div className="flex flex-row items-center game-wrapper">
             <img alt="" src={PhantomLogo} className='object-cover game-logo' />
